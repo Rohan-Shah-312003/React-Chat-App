3f670e258284ac2fa289a9bf001c534b5c4f5a68
Rohan_Shah <rohan312003@gmail.com>
2024-06-06 11:59:12 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Chat from "./Components/chat/Chat"
+import Detail from "./Components/detail/Detail"
+import List from "./Components/list/List"
+
+
+const App = () => {
+  return (
+    <div className='container'>
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  )
+}
+
+export default App
